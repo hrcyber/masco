375315e9d85286986458d9b743adc146b5815096
hrcyber <cyber.zone.nk@gmail.com>
2026-05-15 21:52:12 +0530

    Update Streamlit app title and button message

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 # Page ka title set karne ke liye
-st.title("My First Streamlit App")
+st.title("NASEEM KHAN IS BACK")
 
 # Text display karne ke liye
 st.write("Hello, World!")
 
 # Ek simple button aur interactivity ke liye
 if st.button('Click Me'):
-    st.success("Welcome to Streamlit!")
+    st.success("Welcome to Piro")
